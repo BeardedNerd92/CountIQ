f0d1d9a9e504854045e9f9e9f6af851fd42b912f
BeardedNerd92 <IntMain92@gmail.com>
2026-02-04 13:30:17 -0600

    chore: add items app and env-based secret key

--- inventory/inventory/settings.py
+++ inventory/inventory/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
 import os
 
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "dev-only-change-me")
@@ -41,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'items'
 ]
 
 MIDDLEWARE = [
